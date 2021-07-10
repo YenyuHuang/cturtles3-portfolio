2eb20714895075c15c5af68f6c12822d1dd7254b
Lint Action <lint-action@samuelmeuli.com>
2021-07-10 00:13:57 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,22 @@ import json
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 # from . import db
 # from app.db import get_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -24,11 +28,12 @@ SITE_FOLDER = "static/"
 json_url = os.path.join(SITE_ROOT, SITE_FOLDER, "data.json")
 data = json.load(open(json_url))
 
-#data = json.load(data_file)
-#data_file.close()
+# data = json.load(data_file)
+# data_file.close()
+
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -41,23 +46,24 @@ class UserModel(db.Model):
         return f"<User {self.username}>"
 
 
-#Create URL routes
-@app.route('/')
+# Create URL routes
+@app.route("/")
 def home():
     allUsers = data
     return render_template("home.html", allUsers=allUsers)
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -72,18 +78,19 @@ def register():
     # TODO: Return a restister page
     return render_template("register.html")
 
-@app.route('/login', methods=('GET', 'POST'))
+
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
@@ -92,24 +99,27 @@ def login():
 
     # TODO: Return a login page
 
-    return render_template("login.html")   
+    return render_template("login.html")
 
-@app.route('/health')
+
+@app.route("/health")
 def starting_url():
     status_code = Response(status=200)
     return status_code
 
-#app.run(host="0.0.0.0", port=5000)
 
-#Create URL for each members
-@app.route('/about/<string:name>')
+# app.run(host="0.0.0.0", port=5000)
+
+# Create URL for each members
+@app.route("/about/<string:name>")
 def about(name):
     userData = data[name]
     allUsers = data
-    return render_template("about.html", name=name, userData=userData, allUsers=allUsers)
-
+    return render_template(
+        "about.html", name=name, userData=userData, allUsers=allUsers
+    )
 
 
 if __name__ == "__main__":
     # rid (port="5002") within run function
-    app.run(debug=True) 
+    app.run(debug=True)
